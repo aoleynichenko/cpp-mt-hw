fbb5ca393cf7c91f72ccd0525cfdf1b3e86c1473
aoleynichenko <alexvoleynichenko@gmail.com>
2017-03-19 23:49:56 +0000

    filled sockaddr_in

--- hw04/chat-server.cpp
+++ hw04/chat-server.cpp
@@ -1,12 +1,25 @@
 #include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-int create_master_socket(char* port) {
-  int s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+int create_master_socket(char* port_str) {
+  int s;
+  struct sockaddr_in sock_addr;
+  long int port;
+
+  port = strtol(port_str, nullptr, 10);
+
+  bzero(&sock_addr, sizeof(sock_addr));
+  sock_addr.sin_family = AF_INET;
+  sock_addr.sin_port = htons(port);
+  sock_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+  s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
   return s;
 }
 
